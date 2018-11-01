bba43f902ac72fb32c08282bb023e144fcc93ba5
Filip Zych <navarionek@gmail.com>
2018-11-01 10:16:13 +0100

    Merge

--- app/src/main/java/com/spotifyx/core/entity/Artist.kt
+++ app/src/main/java/com/spotifyx/core/entity/Artist.kt
@@ -1,6 +1,16 @@
 package com.spotifyx.core.entity
 
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity
 data class Artist(
     val name: String,
     val uri: String
 )
+
+
+@Entity
+data class SpotifyPlayerState(
+    @PrimaryKey @ColumnInfo(name = "id") val id: String)
--- app/src/main/java/com/spotifyx/feature/background/LocalDataSynchronizer.kt
+++ app/src/main/java/com/spotifyx/feature/background/LocalDataSynchronizer.kt
@@ -3,7 +3,9 @@ package com.spotifyx.feature.background
 import com.spotify.android.appremote.api.SpotifyAppRemote
 import com.spotify.protocol.types.PlayerState
 import com.spotifyx.core.auth.Authenticator
-import com.spotifyx.core.entity.SpotifyPlayerState
+import com.spotifyx.core.entity.Album
+import com.spotifyx.core.entity.Artist
+import com.spotifyx.core.entity.Track
 import com.spotifyx.core.services.AlarmService
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -39,12 +41,25 @@ class LocalDataSynchronizer
 //        playerState.track.imageUri
 ////        playerState.
 //    }
-
+    
     private fun subscribeToPlayer(remote: SpotifyAppRemote): Observable<PlayerState> {
         return Observable.fromPublisher { subscriber ->
             remote.playerApi.subscribeToPlayerState().setEventCallback { subscriber.onNext(it) }
         }
     }
 
+    private fun mapEntity(playerState: PlayerState) = playerState.track.let {
 
+                val artists: MutableList<Artist> = emptyList<Artist>().toMutableList()
+                it.artists.forEach { artists.add(Artist(it.name, it.uri)) }
+
+            Track(
+                Artist(it.artist.name, it.artist.uri),
+                artists,
+                Album(it.album.name, it.album.uri),
+                it.duration,
+                it.name,
+                it.uri,
+                it.imageUri.raw)
+        }
 }
